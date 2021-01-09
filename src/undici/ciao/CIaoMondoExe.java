b7446e1a10e399429ec26eef3947cc062919c501
Saraswati <federicabarel@gmail.com>
2021-01-09 20:23:20 +0100

    Primo Commit

---
+++ src/undici/ciao/CIaoMondoExe.java
@@ -0,0 +1,10 @@
+package undici.ciao;
+
+public class CIaoMondoExe {
+
+//	public static void main(String[] args) {
+//		// TODO Auto-generated method stub
+//		System.out.println("CIao MONDO");
+//	}
+
+}
---
+++ src/undici/ciao/HolaGitMundo.java
@@ -0,0 +1,10 @@
+package undici.ciao;
+
+public class HolaGitMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("CIao Git MONDO corrotto");
+	}
+
+}
